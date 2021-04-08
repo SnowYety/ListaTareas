fff4f09cf5c4decf69236c505bbaada1ea2d3017
Jesus <jnieves@ucq.edu.mx>
2021-04-08 17:11:25 -0500

    Se agrego la funcionalidad de Borrar y Completar Tarea

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 require('colors');
 const { guardarDB, leerDb } = require('./helpers/guardarArchivo');
 //const {mostarMenu,pausa}= require('./helpers/mensajes');
-const {inquirerMenu,pausa, leerInput} = require('./helpers/inquirer');
+const {inquirerMenu,pausa, leerInput, listadoTareasBorrar,confirmar, mostrarListadoCheksList} = require('./helpers/inquirer');
 const Tarea = require('./models/tarea');
 const Tareas = require('./models/tareas');
 console.clear();
@@ -26,6 +26,27 @@ const main = async()=>{
             break;
             case '2':
                 tareas.listadoCompleto()
+            break;
+            case '3':
+                tareas.listarPendientesCompletadas();
+            break;
+            case '4':
+                tareas.listarPendientesCompletadas(false);
+            break;
+            case '5':
+                const ids = await mostrarListadoCheksList(tareas.listadoArr);
+                tareas.toggleCompletadas(ids);
+            break;
+            case '6':
+                const id =await listadoTareasBorrar(tareas.listadoArr);
+
+                if(id !== '0'){
+                    const ok = await confirmar('Esta seguro de Borrar esta Tarea');
+                    if(ok){
+                        tareas.borrarTarea(id);
+                        console.log('La Tarea se Borro correctamente'.green);
+                    }
+                }
             break;
         }
 
--- models/tareas.js
+++ models/tareas.js
@@ -35,7 +35,51 @@ class Tareas {
             console.log(`${idx}${'.-'.green}${desc} :: ${estado}`);
          
         })
-    
+    }
+
+    listarPendientesCompletadas(completadas = true){
+
+        let contador=0;
+        this.listadoArr.forEach(tarea =>{
+            
+            const {desc,completadoEn} = tarea;
+            const estado = (completadoEn)
+                            ? 'Compleatado'.green
+                            : 'Pendiente'.red
+            if(completadas){
+                if(completadoEn){
+                    contador += 1;
+                    console.log(`${contador.toString().green}${'.-'.green}${desc} :: ${completadoEn.green}`);
+                }
+            }else{
+                if(!completadoEn){
+                    contador += 1;
+                    console.log(`${contador.toString().green}${'.-'.green}${desc} :: ${estado}`);
+                }
+            }
+        })
+    }
+
+    borrarTarea(id=''){
+        if(this._listado[id]){
+
+            delete this._listado[id];
+        }
+    }
+
+    toggleCompletadas(ids=[]){
+        ids.forEach(id =>{
+            const tarea = this._listado[id];
+            if(!tarea.completadoEn){
+                tarea.completadoEn = new Date().toISOString();
+            }
+        })
+
+        this.listadoArr.forEach(tarea => {
+            if(!ids.includes(tarea.id)){
+                this._listado[tarea.id].completadoEn = null;
+            }
+        })
     }
 }
 module.exports = Tareas;
